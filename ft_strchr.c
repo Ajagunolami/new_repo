d0aec00eba3c307d7d3d1e6a4a525ea53cef6794
Ajagunolami <ajagunolami@gmail.com>
2023-09-02 09:50:21 +0400

    libft

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,17 +1,137 @@
 #include "libft.h"
 
-char *ft_strrchr(const char *str, int c)
+void *ft_strrchr(const char *str, int c)
 {
 	int i;
 
 	i = 0;
-	while (str[i] != '\0')
-		i++;
-	while (i > 0)
+	while (str[i] != c)
 	{
-		if (str[i] == (char)c)
-			return ((char *)(str + i));
-		i--;
+		if (str[i] == '\0')
+		{
+			return (NULL);
+		}
+		i++;
+		str++;
 	}
+	return (str);
+}
+ int		main(void)
+{
+	char str[] = "Lucas";
+	char	a;
+
+	a = 'u';
+	ft_strchr(str, a);
+	printf("%s", (char *)ft_strchr(str, a));
+	printf("\n");
+	printf("%s", strchr(str, a));
 	return (0);
 }
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+ft_strchr.c[+] [unix] (13:40 31/08/2023)                                                                                                                                                                                                                                  19,1 All
+-- INSERT --
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+ft_strchr.c[+] [unix] (13:40 31/08/2023)                                                                                                                                                                                                                                  19,1 All
+-- INSERT --
+
